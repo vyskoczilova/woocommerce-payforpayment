9aa8def2af6669255c323f8b5975c901467a4826
Karolína Vyskočilová <karolina@kybernaut.cz>
2018-10-13 22:15:58 +0200

    update WC version

--- woocommerce-payforpayment.php
+++ woocommerce-payforpayment.php
@@ -11,7 +11,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 Text Domain: woocommerce-pay-for-payment
 Domain Path: /languages
 WC requires at least: 2.6
-WC tested up to: 3.4.5
+WC tested up to: 3.4.6
 */
 
 /**
